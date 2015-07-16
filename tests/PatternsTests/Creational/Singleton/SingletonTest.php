4f1a71830b77d598d1ef5f5e9fbfd220d79fc2a2
Dmitry Vapelnik <dvapelnik@gmail.com>
2015-07-16 17:09:44 +0300

    Singleton class and trait fully covered

--- tests/PatternsTests/Creational/Singleton/SingletonTest.php
+++ tests/PatternsTests/Creational/Singleton/SingletonTest.php
@@ -1,9 +1,11 @@
 <?php
 namespace PatternsTests\Creational\Singleton;
 
+use Patterns\Creational\Singleton\Singleton;
 use Patterns\Creational\Singleton\SingletonA;
 use Patterns\Creational\Singleton\SingletonB;
 use PHPUnit_Framework_TestCase;
+use ReflectionMethod;
 
 class SingletonTest extends PHPUnit_Framework_TestCase
 {
@@ -30,5 +32,27 @@ class SingletonTest extends PHPUnit_Framework_TestCase
 
         $singletonB = SingletonB::GetInstance();
         $this->assertInstanceOf('Patterns\Creational\Singleton\SingletonB', $singletonB);
+    }
+
+    public function testPrivateCloneMethod()
+    {
+        // Only for fully covering code by tests
+        $singleton = Singleton::getInstance();
+
+        $reflectedSingletonCloneMethod = new ReflectionMethod('\Patterns\Creational\Singleton\Singleton', '__clone');
+        $reflectedSingletonCloneMethod->setAccessible('true');
+
+        $reflectedSingletonCloneMethod->invoke($singleton);
+    }
+
+    public function testPrivateWakeUpMethod()
+    {
+        // Only for fully covering code by tests
+        $singleton = Singleton::getInstance();
+
+        $reflectedSingletonWakeUpMethod = new ReflectionMethod('\Patterns\Creational\Singleton\Singleton', '__wakeup');
+        $reflectedSingletonWakeUpMethod->setAccessible('true');
+
+        $reflectedSingletonWakeUpMethod->invoke($singleton);
     }
 }
